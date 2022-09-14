9ef0cbd9db34403f541e65587a18b5de4c77c064
Shavrin Dmitry <shavrin.dmitrii@otr.ru>
2022-09-14 13:30:26 +0300

    ngx-10

---
+++ ngx-10/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#не удалось подключить mitogen 0.3.0-0.3.3 к ansible 2.10, 2.11
+#export ANSIBLE_STRATEGY=mitogen_linear 
+#export ANSIBLE_STRATEGY_PLUGINS=/home/dmitry/.local/lib/python3.8/site-packages/ansible_mitogen/plugins/strategy
+
+ansible-playbook -i inventories/rbr build_nginx.yml 
+ansible-playbook -i inventories/rbr deploy_nginx.yml
+ansible-playbook -i inventories/rbr test_nginx.yml
+
+sleep 1
+echo "run 'forkstat -e all -S' in case of any problems"
